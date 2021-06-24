83e2406ace176c06042f9376aa51e1ee2899bf1e
Katherine Jackson <katherine.jackson206@gmail.com>
2021-06-24 16:39:46 -0500

    fixed syntax errors

--- schema.sql
+++ schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE `department`(
 CREATE TABLE `role`(
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INT,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
@@ -19,8 +19,8 @@ CREATE TABLE `employee`(
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE SET NULL
+  role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE SET NULL,
   manager_id INT,
       FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
 )
